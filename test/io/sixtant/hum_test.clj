96592681cf712b2010253d48824f589e723bcedc
Matthew Downey <matthewdowney20@gmail.com>
2021-02-18 12:55:47 -0600

    Update test cases to include an overflow condition

--- test/io/sixtant/hum_test.clj
+++ test/io/sixtant/hum_test.clj
@@ -1,20 +1,19 @@
 (ns io.sixtant.hum-test
   (:require [clojure.test :refer :all]
             [io.sixtant.hum :refer :all]
-            [io.sixtant.hum.messages :as messages]
-            [clojure.java.io :as io]))
+            [io.sixtant.hum.messages :as messages]))
 
 
-(def micro-timestamp (* (System/currentTimeMillis) 1000))
+(def timestamp (System/currentTimeMillis))
 
 
 (def snap
   (messages/order-book-snapshot
     {:tick-size 0.01M
-     :lot-size 0.000001M
-     :bids [{:price 100000.52M :qty 1.2345M}]
-     :asks [{:price 102000.52M :qty 50.2345M}]
-     :timestamp micro-timestamp}))
+     :lot-size  0.000001M
+     :bids      [{:price 100000.52M :qty 1.2345M}]
+     :asks      [{:price 102000.52M :qty 50.2345M}]
+     :timestamp timestamp}))
 
 
 (def diff-a
@@ -22,7 +21,7 @@
     {:price 125000.01M
      :qty 20.3045M
      :bid? false
-     :timestamp (+ micro-timestamp 100)}))
+     :timestamp (+ timestamp 100)}))
 
 
 (def diff-b
@@ -30,19 +29,51 @@
     {:price 102000.52M
      :qty 2.35M
      :bid? true
-     :timestamp (+ micro-timestamp 200)}))
+     :timestamp (+ timestamp 200)}))
+
+
+(def rem-a (-> (assoc diff-a :qty 0M) (update :timestamp + 200)))
+(def rem-b (-> (assoc diff-b :qty 0M) (update :timestamp + 200)))
 
 
 (deftest serialization-test
-  (is (= (->> [snap diff-a diff-b]
-              (write-with writer1)
-              (read-with reader1))
-         [snap diff-a diff-b])
+  (is (= (->> [snap diff-a diff-b rem-a rem-b]
+              (write-with writer)
+              (read-with reader))
+         [snap diff-a diff-b rem-a rem-b])
       "Messages survive the serialize -> deserialize loop intact.")
 
-  (let [empty-snap (write-with writer1 [(assoc snap :bids [] :asks [])])]
+  (let [empty-snap (write-with writer [(assoc snap :bids [] :asks [])])]
     (println (count empty-snap) "bytes / empty snapshot"))
 
-  (let [snap-bytes (write-with writer1 [snap])
-        snap+diff (write-with writer1 [snap diff-a])]
-    (println (- (count snap+diff) (count snap-bytes)) "bytes / diff")))
+  (let [snap-bytes (write-with writer [snap])
+        snap+diff (write-with writer [snap diff-a])]
+    (println (- (count snap+diff) (count snap-bytes)) "bytes / diff"))
+
+  (let [snap-bytes (write-with writer [snap])
+        snap+rem (write-with writer [snap rem-a])]
+    (println (- (count snap+rem) (count snap-bytes)) "bytes / removal")))
+
+
+(def big-number 100000000000000000000000000000000000000000000000000000000000M)
+
+
+(deftest overflow-test
+  (doseq [attr [:price :qty]]
+    (testing (format "when %s overflows" (name attr))
+
+      (testing "and there is no snapshot-delay"
+        (let [diff (assoc diff-a attr big-number)]
+          (is
+            (thrown?
+              IllegalArgumentException
+              #"Diffs must include a :snapshot-delay so that.*"
+              (write-with writer [snap diff])))))
+
+      (testing "and there is a snapshot-delay"
+        (let [diff (assoc diff-a attr big-number :snapshot-delay (delay snap))]
+          (is (= (->> [snap diff]
+                      (write-with writer)
+                      (read-with reader))
+                 [snap (assoc snap :timestamp (:timestamp diff))])
+              "a new snapshot is written instead of the invalid diff"))))))
